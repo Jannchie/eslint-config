ed2b28d7e5f5a28e0f8832f4ec2bab0b53e8c638
Jannchie <jannchie@gmail.com>
2023-02-14 11:21:42 +0900

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  rules: {
+    indent: ['warn', 2, { SwitchCase: 1 }],
+    '@typescript-eslint/indent': ['warn', 2, { SwitchCase: 1 }],
+    'jsx-quotes': ['warn', 'prefer-double'],
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'warn',
+    'comma-dangle': ['warn', 'always-multiline'],
+    '@typescript-eslint/comma-dangle': ['warn', 'always-multiline'],
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/self-closing-comp': ['warn', { component: true, html: true }],
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        noSortAlphabetically: true,
+        callbacksLast: true,
+        reservedFirst: true,
+        shorthandFirst: true,
+      },
+    ],
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+  ],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'plugin:jsx-a11y/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
