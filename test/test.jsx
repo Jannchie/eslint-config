7dda16ba0d7e8bb16934b095b29ee16d5720e163
Jannchie <jannchie@gmail.com>
2023-04-30 12:24:43 +0900

    feat: add jsx rules

--- test/test.jsx
+++ test/test.jsx
@@ -1,9 +1,12 @@
 export function a () {
   return (
     <>
-      <div className={`123${1}`} style={{
-        color: 'red',
-      }} />
+      <div
+        className={`123${1}`}
+        style={{
+          color: 'red',
+        }}
+      />
       213
       <div />
     </>
