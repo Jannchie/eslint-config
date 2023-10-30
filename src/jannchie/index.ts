7565ea3c5d7aa1210e14a7ef1fc1b596b4d627a8
Jianqi Pan <jannchie@gmail.com>
2023-10-30 18:40:26 +0900

    :adhesive_bandage: fix(jannchie): fix vue

--- src/jannchie/index.ts
+++ src/jannchie/index.ts
@@ -1,12 +1,16 @@
 import { pluginStylistic } from 'src/plugins'
 import { GLOB_SRC, GLOB_VUE } from '../globs'
 import type { ConfigItem } from '../types'
+import { pluginVue } from '../plugins'
 
 export function jannchie(): ConfigItem[] {
   return [
     {
       files: [GLOB_VUE],
       name: 'jannchie:vue',
+      plugins: {
+        vue: pluginVue,
+      },
       rules: {
         'vue/max-attributes-per-line': ['error', {
           multiline: {
