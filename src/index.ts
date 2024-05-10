e1eb19b76dc545808dc5197d11cb43000d7549b5
Jianqi Pan <jannchie@gmail.com>
2024-05-10 20:08:43 +0900

    :sparkles: feat(vue): add rules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import antfu, { GLOB_SRC, GLOB_VUE, type TypedFlatConfigItem } from '@antfu/eslint-config'
+import antfu, { GLOB_SRC, GLOB_VUE, type OptionsConfig, type TypedFlatConfigItem } from '@antfu/eslint-config'
 
-const jannchieRules: TypedFlatConfigItem[] = [
-  {
-    files: [GLOB_VUE],
-    name: 'jannchie:vue',
-    rules: {
+const defaultOptions: OptionsConfig = {
+  vue: {
+    overrides: {
+      'vue/mustache-interpolation-spacing': ['error', 'always'],
+      'vue/no-multi-spaces': ['error'],
       'vue/max-attributes-per-line': ['error', {
         multiline: {
           max: 1,
@@ -12,6 +12,9 @@ const jannchieRules: TypedFlatConfigItem[] = [
       }],
     },
   },
+}
+
+const jannchieRules: TypedFlatConfigItem[] = [
   {
     files: [GLOB_SRC, GLOB_VUE],
     name: 'jannchie:src',
@@ -19,11 +22,13 @@ const jannchieRules: TypedFlatConfigItem[] = [
       'curly': ['error', 'multi-line'],
       'import/no-mutable-exports': 'off',
       'style/brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+      '@stylistic/js/comma-dangle': ['error', 'always'],
     },
   },
 ]
 export default function (...params: Parameters<typeof antfu>) {
-  if (params.length === 0)
-    return antfu({}, jannchieRules)
-  return antfu(...params, jannchieRules)
+  if (params.length === 0) {
+    return antfu({ ...defaultOptions }, jannchieRules)
+  }
+  return antfu({ ...defaultOptions, ...params }, jannchieRules)
 };
